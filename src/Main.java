a0cd63fe63cd8b82db9e3ac07030226b0371df9f
kyanh <nguyenmanhkyanh3112@gmail.com>
2024-11-09 15:08:44 +0700

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello User");
-        System.out.println("This line use to test");
+        System.out.println("this line use to test");
     }
 }
